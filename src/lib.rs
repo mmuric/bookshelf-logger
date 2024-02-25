8758e3206abd04f65ff8ed69cf64db8fdaff9576
Sasha Shipka <xaotuk@gmail.com>
2024-02-25 18:20:32 +0100

    fixes for lib code

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,36 @@
 use chrono::prelude::*;
 use neon::prelude::*;
 
-
 pub fn print_log_entry(uuid: &str, msrv: &str, comment: &str, payload: &serde_json::Value) {
-  let current_time = Local::now();
-  let formatted_time = current_time.format("%Y-%m-%d %H:%M").to_string();
-  
-  println!("[\x1b[1m{}\x1b[0m]: \x1b[0;32m{}\x1b[0m (\x1b[1;34m{}\x1b[0m) - {} => {}", formatted_time, uuid, msrv, comment, payload);
+    let current_time = Local::now();
+    let formatted_time = current_time.format("%Y-%m-%d %H:%M").to_string();
+
+    println!(
+        "[\x1b[1m{}\x1b[0m]: \x1b[0;32m{}\x1b[0m (\x1b[1;34m{}\x1b[0m) - {} => {}",
+        formatted_time, uuid, msrv, comment, payload
+    );
 }
 
-pub fn print_log_entry_js(cx: FunctionContext, uuid_js: JsString, msrv_js: JsString, payload_js: JsObject) {
-  // let uuid = uuid_js.
+pub fn print_log_entry_js(mut cx: FunctionContext) -> JsResult<JsUndefined> {
+    let uuid = cx.argument::<JsString>(0)?.value(&mut cx);
+    let msrv = cx.argument::<JsString>(1)?.value(&mut cx);
+    let comment = cx.argument::<JsString>(2)?.value(&mut cx);
+    let payload = cx
+        .argument::<JsValue>(3)?
+        .to_string(&mut cx)?
+        .value(&mut cx);
+    print_log_entry(
+        &uuid,
+        &msrv,
+        &comment,
+        &payload.parse::<serde_json::Value>().unwrap(),
+    );
+    Ok(cx.undefined())
 }
 
 #[neon::main]
 fn main(mut cx: ModuleContext) -> NeonResult<()> {
-    // cx.export_function("hello", hello)?;
-    cx.export_function("print_log_entry_js", print_log_entry_js)?;
+    cx.export_function("print_log_entry", print_log_entry_js)?;
     Ok(())
 }
+
